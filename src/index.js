e46dbbec4ee17359309b69f2c234bca55a7336ee
KYRYLO VOITOVSKYI <kirill.voitovskiy@gmail.com>
2021-09-27 14:31:26 +0300

    upgrade

--- src/index.js
+++ src/index.js
@@ -1,1 +1,3 @@
 import './sass/main.scss';
+import './js/create-cards-set';
+import './js/theme';
---
+++ src/js/create-cards-set.js
@@ -0,0 +1,9 @@
+import cardsTemplate from '../templates/cards.hbs';
+
+import menu from '../menu.json';
+
+const markup = cardsTemplate(menu);
+
+
+const menuRef = document.querySelector('.js-menu');
+menuRef.insertAdjacentHTML('beforeend', markup); 
